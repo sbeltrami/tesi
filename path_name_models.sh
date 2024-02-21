6e54c5fbc78dc77dea024a4e8494b91d0db936c3
sara <sara.beltrami@edu.unito.it>
2024-02-21 16:26:22 +0100

    bash script: models' name and paths

--- path_name_models.sh
+++ path_name_models.sh
@@ -10,10 +10,13 @@ modeloutput=/work/users/guest/sbeltrami
 
 #per assicurarmi che i file siano vuoti all'inizio
 echo -n > name_model.txt #file in cui scrivo il nome di tutti i modelli
-echo -n > tos_nc_model.txt #file in cui scrivo il percorso dei modelli che hanno modulo oceano
-echo -n > remap_tos_nc_model.txt #file in cui scrivo il percorso dei modelli che hanno modulo oceano e sono stati rimappati
-echo -n > name_model_ocean.txt #file in cui scrivo il nome dei modelli che hanno modulo oceano
-echo -n > no_ocean.txt #file in cui scrivo il nome dei modelli che non hanno modulo oceano
+echo -n > name_ocean_model.txt #file in cui scrivo il nome dei modelli che hanno modulo oceano
+echo -n > path_ocean_model.txt #file in cui scrivo il percorso dei modelli che hanno modulo oceano
+echo -n > path_remap_ocean_model.txt #file in cui scrivo il percorso dei modelli che sono stati rimappati
+echo -n > remapcon_model.txt #file in cui scrivo il nome dei modelli che hanno modulo oceano e sono stati rimappati con remapcon
+echo -n > remapbil_model.txt #file in cui scrivo il nome dei modelli che hanno modulo oceano e sono stati rimappati con remapbil
+echo -n > remapnn_model.txt #file in cui scrivo il nome dei modelli che hanno modulo oceano e sono stati rimappati con remapnn
+echo -n > name_no_ocean_model.txt #file in cui scrivo il nome dei modelli che non hanno modulo oceano
 
 for modelpath in $models ; do
 
@@ -26,24 +29,44 @@ for modelpath in $models ; do
         modelfiles=$modelpath/historical/ocean/Omon/r1i1p1f1/tos/tos_Omon_${model}_historical_r1i1p1f1_*.nc
         
         #echo $(ls $modelfiles)
-        if [ -e $modelfiles ]; then #se il percorso esiste
+        #if [ -e $modelfiles ]; then #se il percorso esiste
+        if ls $modelfiles 1> /dev/null 2>&1; then #prendo più file .nc nel percorso, non soltanto uno
 
-            printf "%s\n" $model >> name_model_ocean.txt #stampo su file il nome del modello con oceano 
+            printf "%s\n" $model >> name_ocean_model.txt #stampo su file il nome del modello con modulo oceano 
 
             cdo -cat $modelfiles ${modeloutput}/${model}.nc #concatena
 
-            printf "%s\n" ${modeloutput}/${model}.nc >> tos_nc_model.txt #scrivo su file il nome .nc
+            printf "%s\n" ${modeloutput}/${model}.nc >> path_ocean_model.txt #scrivo su file il nome .nc
 
-            cdo remapbil,r180x90 -selname,tos ${modeloutput}/${model}.nc ${modeloutput}/${model}_remap.nc
-            if [ $? -eq 0 ]; then #se il comando precedente non è stato eseguito con successo
-                cdo remapcon,r180x90 -selname,tos ${modeloutput}/${model}.nc ${modeloutput}/${model}_remap.nc
+            #remapcon
+            cdo remapcon,r180x90 -selname,tos ${modeloutput}/${model}.nc ${modeloutput}/${model}_remapcon.nc           
+            if [ $? -eq 0 ]; then #se il comando è stato eseguito correttamente allora scrivo su file
+                printf "%s\n" ${modeloutput}/${model}_remapcon.nc >> path_remap_ocean_model.txt
+                printf "%s\n" $model >> remapcon_model.txt #scrivo su file il nome .nc
+            else #se il comando precedente non è stato eseguito con successo
+                rm ${modeloutput}/${model}_remapcon.nc #rimuovo quanto creato perché non è andato a buon fine il processo
+                #remapbil
+                cdo remapbil,r180x90 -selname,tos ${modeloutput}/${model}.nc ${modeloutput}/${model}_remapbil.nc
+                if [ $? -eq 0 ]; then 
+                    printf "%s\n" ${modeloutput}/${model}_remapbil.nc  >> path_remap_ocean_model.txt
+                    printf "%s\n" $model >> remapbil_model.txt #scrivo su file il nome .nc
+                else #se il comando precedente non è stato eseguito con successo
+                    rm ${modeloutput}/${model}_remapbil.nc
+                    #remapnn
+                    cdo remapnn,r180x90 -selname,tos ${modeloutput}/${model}.nc ${modeloutput}/${model}_remapnn.nc
+                    if [ $? -eq 0 ]; then  
+                        printf "%s\n" ${modeloutput}/${model}_remapnn.nc  >> path_remap_ocean_model.txt
+                        printf "%s\n" $model >> remapnn_model.txt #scrivo su file il nome .nc
+                    else
+                        rm ${modeloutput}/${model}_remapnn.nc
+                        echo "${model} non rimappabile con i tre remap scelti"
+                    fi
+                fi                
             fi
-
-            printf "%s\n" ${modeloutput}/${model}_remap.nc >> remap_tos_nc_model.txt #scrivo su file il nome .nc
 
         else
 
-            printf "%s\n" "$model non contiene il modulo ocean" >> no_ocean.txt
+            printf "%s\n" $model >> name_no_ocean_model.txt #modelli che non contengono il modulo oceano
         fi
 
 
