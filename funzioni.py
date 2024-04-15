a3edc2480aa41b9efe4b28f27b929b6fc9836eea
sara <sara.beltrami@edu.unito.it>
2024-04-15 13:04:08 +0200

    functions

--- funzioni.py
+++ funzioni.py
@@ -51,6 +51,7 @@ def compute_mean_time_season(dataset,seas):
 def plot_bias_tos(n_rows,n_cols,fig_size,number_models,name_models_to_plot,name_dict,val_min,val_max,title_plot,title_pdf): #number_models è la lista che riporta il numero di modelli in un determinato cluster
     #Plot dei modelli
     fig, ax = plt.subplots(nrows=n_rows,ncols=n_cols,figsize=fig_size)
+    fig.subplots_adjust(hspace=0.5)  # Aggiungo spazi verticali tra le subplots
     # Plot dei modelli
     for i in range(n_rows): #ciclo sulle righe 
         for j in range(n_cols): #ciclo sulle colonne 
@@ -64,9 +65,7 @@ def plot_bias_tos(n_rows,n_cols,fig_size,number_models,name_models_to_plot,name_
             #ax[i,j].legend(loc='upper right')
             ax[i,j].set_ylabel('latitude')
             ax[i,j].set_xlabel('longitude')
-            
-            # Aggiungi il nome del modello in alto a destra
-            ax[i,j].text(0.95, 0.95, model_name, horizontalalignment='right', verticalalignment='top', transform=ax[i,j].transAxes, fontsize=10, color='black')
+            ax[i,j].set_title(model_name) #nome di ogni singolo modello sul plot
 
     #Rimuovo i quadrati non utilizzati
     for i in range(n_rows):
@@ -93,9 +92,7 @@ def plot_bias_2_models_tos(fig_size,val_min,val_max,name_models_to_plot,name_dic
         plot_mod.set_clim(vmin=val_min, vmax=val_max)
         ax[i].set_ylabel('latitude')
         ax[i].set_xlabel('longitude')
-            
-        # Aggiungi il nome del modello in alto a destra
-        ax[i].text(0.95, 0.95, model_name, horizontalalignment='right', verticalalignment='top', transform=ax[i].transAxes, fontsize=10, color='black')
+        ax[i].set_title(model_name) #nome di ogni singolo modello sul plot
 
 
     # Rimuovi i quadrati non utilizzati
@@ -122,7 +119,7 @@ def plot_mean_cluster_tos(number_models,name_models_to_plot,name_dict,title_plot
     #valor medio
     mean_bias = sum_bias / number_models
     #plot del valor medio per il cluster 3
-    plot_mod = mean_bias.plot.pcolormesh(figsize=fig_size,vmin=v_min, vmax=v_max)
+    plot_mod = mean_bias.plot.pcolormesh(figsize=fig_size,vmin=v_min, vmax=v_max,cmap='coolwarm')
     plt.ylabel('latitude')
     plt.xlabel('longitude')
     # Titolo
@@ -135,6 +132,7 @@ def plot_mean_cluster_tos(number_models,name_models_to_plot,name_dict,title_plot
 #funzione per plot bias atmos
 def plot_bias_atmos(n_rows,n_cols,fig_size,v_min,v_max,name_models_to_plot,name_dict,dataset_seas_mean,title_plot,title_pdf): #name_models_to_plot indica la lista in cui sono racchiusi i nomi dei modelli, name_dict è model_atmos, val_min e max sono i valori che fissano la scala, dataset_seas_mean è era_na_seas_mean
     fig, ax = plt.subplots(nrows=n_rows,ncols=n_cols,figsize=fig_size)
+    fig.subplots_adjust(hspace=0.5)  # Aggiungo spazi verticali tra le subplots
 
     for i in range(n_rows): #ciclo sulle righe
         for j in range(n_cols): #ciclo sulle colonne
@@ -156,9 +154,7 @@ def plot_bias_atmos(n_rows,n_cols,fig_size,v_min,v_max,name_models_to_plot,name_
             #label assi
             ax[i,j].set_ylabel('lat')
             ax[i,j].set_xlabel('lon')
-            
-            # Aggiungi il nome del modello in alto a destra
-            ax[i,j].text(0.95, 0.95, model_name, horizontalalignment='right', verticalalignment='top', transform=ax[i,j].transAxes, fontsize=10, color='black')
+            ax[i,j].set_title(model_name) #nome di ogni singolo modello sul plot
 
     #Rimuovo i quadrati non utilizzati
     for i in range(n_rows):
@@ -180,7 +176,7 @@ def plot_bias_2_models_atmos(fig_size,v_min,v_max,name_models_to_plot,name_dict,
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=fig_size)  # Modificato per 2 righe e 1 colonna
 
     # Plot dei modelli
-    for i in range(2):  # Solo 2 righe
+    for i in range(2):  # Solo 2 colonne
         model_name = name_models_to_plot[i]  # Usa l'indice i direttamente
         data_array = name_dict[model_name]['atmos North Atlantic bias DJF']
         plot_mod = data_array[0].plot.pcolormesh(ax=ax[i])
@@ -195,9 +191,8 @@ def plot_bias_2_models_atmos(fig_size,v_min,v_max,name_models_to_plot,name_dict,
         data_era[0].plot.contour(ax=ax[i],colors='g')
         #label assi
         ax[i].set_ylabel('latitude')
-        ax[i].set_xlabel('longitude')        
-        # Aggiungi il nome del modello in alto a destra
-        ax[i].text(0.95, 0.95, model_name, horizontalalignment='right', verticalalignment='top', transform=ax[i].transAxes, fontsize=10, color='black')
+        ax[i].set_xlabel('longitude')
+        ax[i].set_title(model_name) #nome di ogni singolo modello sul plot
 
     # Rimuovi i quadrati non utilizzati
     for i in range(2):
@@ -222,9 +217,108 @@ def plot_mean_cluster_atmos(name_models_to_plot,name_dict,title_plot,title_pdf,v
     #valor medio
     mean_bias = sum_bias / len(name_models_to_plot)
     #plot del valor medio per il cluster 3
-    plot_mod = mean_bias[0].plot.pcolormesh(figsize=fig_size,vmin=v_min, vmax=v_max)
+    plot_mod = mean_bias[0].plot.pcolormesh(figsize=fig_size,vmin=v_min, vmax=v_max,cmap='coolwarm')
     plt.ylabel('latitude')
     plt.xlabel('longitude')
+    # Titolo
+    plt.suptitle(title_plot, fontsize=16, y=1.02)
+
+    plt.savefig(title_pdf, format='pdf')
+
+#plot della standard deviation dei cluster di atmos
+def plot_std_cluster_atmos(name_models_to_plot,name_dict,title_plot,title_pdf): #name_dict è models_atmos
+    dataset = [] #Inizializzo una lista
+    for i in range(len(name_models_to_plot)): #Vado ad inserire all'interno di dataset tutti gli elementi 'atmos North Atlantic bias DJF' della j-esima lista, dove j = 0,...,4
+        dataset.append(name_dict[name_models_to_plot[i]]['atmos North Atlantic bias DJF'])
+
+    combined_data = xr.concat(dataset, dim='time') #concateno tutti gli elementi all'interno di dataset, lungo la dimensione time
+    std_dev = combined_data.std(dim='time') #calcolo la deviazione standard lungo la dimensione time
+    # Plot
+    std_dev.plot(cmap='coolwarm')
+    # Titolo
+    plt.suptitle(title_plot, fontsize=16, y=1.02)
+
+    plt.savefig(title_pdf, format='pdf')
+
+#plot medie zonali
+def plot_zonavg(n_rows,n_cols,fig_size,name_models_to_plot,name_dict,v_min,v_max,title_plot,title_pdf):
+    #plot medie annuali dei modelli
+    fig, ax = plt.subplots(nrows=n_rows,ncols=n_cols,figsize=fig_size)
+    fig.subplots_adjust(hspace=0.5)  # Aggiungo spazi verticali tra le subplots
+    # Plot dei modelli
+    for i in range(n_rows): #ciclo sulle righe
+        for j in range(n_cols): #ciclo sulle colonne
+            models_index_list = i * n_cols + j #indice del modello all'interno della lista
+            if models_index_list == len(name_models_to_plot):
+                break
+            model_name = name_models_to_plot[models_index_list]
+            data_array = name_dict[model_name]['zonavg mean 1982 - 2014']
+            plot_mod = data_array.plot(ax=ax[i, j])
+            # Fisso la scala
+            plot_mod.set_clim(vmin=v_min, vmax=v_max)
+            ax[i,j].set_ylabel('plev')
+            ax[i,j].set_xlabel('lat')
+            ax[i,j].set_title(model_name) #nome di ogni singolo modello sul plot
+            # Inverto l'asse y in modo t.c i livelli di pressione siano corretti
+            ax[i,j].invert_yaxis()
+
+    #Rimuovo i quadrati non utilizzati
+    for i in range(n_rows):
+        for j in range(n_cols):
+            models_index_list = i * n_cols + j
+            if models_index_list >= len(model_name):
+                ax[i, j].axis('off')
+
+    # Titolo
+    fig.suptitle(title_plot, fontsize=16, y=1.02)
+
+    fig.savefig(title_pdf, format='pdf')
+
+#plot medie zonali per due cluster
+def plot_zonavg_2_cluster(fig_size,name_models_to_plot,name_dict,v_min,v_max,title_plot,title_pdf):
+    #plot medie annuali dei modelli
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=fig_size)  # Modificato per 2 righe e 1 colonna
+    fig.subplots_adjust(hspace=0.5)  # Aggiungo spazi verticali tra le subplots
+    # Plot dei modelli
+    for i in range(2): #ciclo sulle colonne
+        model_name = name_models_to_plot[i]
+        data_array = name_dict[model_name]['zonavg mean 1982 - 2014']
+        plot_mod = data_array.plot(ax=ax[i])
+        # Fisso la scala
+        plot_mod.set_clim(vmin=v_min, vmax=v_max)
+        ax[i].set_ylabel('plev')
+        ax[i].set_xlabel('lat')
+        ax[i].set_title(model_name) #nome di ogni singolo modello sul plot
+        # Inverto l'asse y in modo t.c i livelli di pressione siano corretti
+        ax[i].invert_yaxis()
+
+    # Rimuovo i quadrati non utilizzati
+    for i in range(2):
+        if i >= len(name_models_to_plot):  # Modificato per usare solo l'indice i
+            ax[i].axis('off')  
+
+    # Titolo
+    fig.suptitle(title_plot, fontsize=16, y=1.02)
+
+    fig.savefig(title_pdf, format='pdf')
+
+#plot dei cluster medi per medie zonali
+
+#funzione per il plot dei cluster medi tos
+def plot_mean_cluster_zonavg(number_models,name_models_to_plot,name_dict,title_plot,title_pdf,v_min,v_max,fig_size):
+    #Inizializzo sum_zonavg per il calcolo della media
+    sum_zonavg = 0
+    #calcolo il valor medio
+    for i in range(number_models):
+        model_name = name_models_to_plot[i]
+        sum_zonavg = sum_zonavg + name_dict[model_name]['zonavg mean 1982 - 2014']
+    #valor medio
+    mean_zonavg = sum_zonavg / number_models
+    #plot del valor medio
+    mean_zonavg.plot(figsize=fig_size,vmin=v_min, vmax=v_max,cmap='coolwarm')
+    plt.ylabel('latitude')
+    plt.xlabel('longitude')
+    plt.gca().invert_yaxis()
     # Titolo
     plt.suptitle(title_plot, fontsize=16, y=1.02)
 
